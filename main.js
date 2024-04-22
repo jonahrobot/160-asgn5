4b473e53254ce0e886d044ddcf9f502627b62048
Jonah Ryan <jonahrobot@gmail.com>
2024-04-21 22:40:42 -0700

    Final 5A

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 import * as THREE from 'three';
+import * as LOADER from 'loader';
+import * as MTL from 'MTLLoader';
 
 function main() {
 
@@ -8,9 +10,9 @@ function main() {
 	const fov = 75;
 	const aspect = 2; // the canvas default
 	const near = 0.1;
-	const far = 5;
+	const far = 10;
 	const camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
-	camera.position.z = 2;
+	camera.position.z = 5;
 
 	const scene = new THREE.Scene();
 
@@ -23,43 +25,87 @@ function main() {
 		scene.add( light );
 
 	}
+    
+    let man = null;
 
-	const boxWidth = 1;
-	const boxHeight = 1;
-	const boxDepth = 1;
-	const geometry = new THREE.BoxGeometry( boxWidth, boxHeight, boxDepth );
+
+    // Little Man by Don Carson [CC-BY] via Poly Pizza
+    {
+        const objLoader = new LOADER.OBJLoader();
+        const mtlLoader = new MTL.MTLLoader();
+        mtlLoader.load('LittleMan/materials.mtl', (mtl) => {
+          mtl.preload();
+          objLoader.setMaterials(mtl);
+          objLoader.load('LittleMan/model.obj', (root) => {
+            man = root;
+            scene.add(root);
+            root.position.x = 0;
+            root.position.y = 0;
+            root.position.z = 4;
+            root.scale.set(1,1,1)
+            root.rotation.y = 3;
+          });
+        });
+      }
+
+      
+      
+	const sphereGeometry = new THREE.SphereGeometry( 1, 5, 4 );
+    const BoxGeometry = new THREE.BoxGeometry(1, 1, 1);
+    const coneGeometry = new THREE.ConeGeometry( 1, 2, 5 );
 
 	function makeInstance( geometry, color, x ) {
 
 		const material = new THREE.MeshPhongMaterial( { color } );
 
-		const cube = new THREE.Mesh( geometry, material );
-		scene.add( cube );
+		const inst = new THREE.Mesh( geometry, material );
+		scene.add( inst );
 
-		cube.position.x = x;
+		inst.position.x = x;
 
-		return cube;
+		return inst;
 
 	}
 
-	const cubes = [
-		makeInstance( geometry, 0x44aa88, 0 ),
-		makeInstance( geometry, 0x8844aa, - 2 ),
-		makeInstance( geometry, 0xaa8844, 2 ),
+    const loader = new THREE.TextureLoader();
+    const texture = loader.load( 'wall.jpg' );
+    texture.colorSpace = THREE.SRGBColorSpace;
+
+    function makeTexturedInstance( geometry, color, x){
+        const material = new THREE.MeshBasicMaterial( { map: texture } );
+
+		const inst = new THREE.Mesh( geometry, material );
+		scene.add( inst );
+
+		inst.position.x = x;
+
+		return inst;
+    }
+
+	const shapes = [
+        makeTexturedInstance( BoxGeometry, 0x44aa88, -5 ),
+		makeInstance( coneGeometry, 0x8844aa, - 3 ),
+		makeInstance( coneGeometry, 0xaa8844, 3 ),
+        makeTexturedInstance( BoxGeometry, 0x8844aa, 5 ),
 	];
 
 	function render( time ) {
 
 		time *= 0.001; // convert time to seconds
 
-		cubes.forEach( ( cube, ndx ) => {
+		shapes.forEach( ( cube, ndx ) => {
 
 			const speed = 1 + ndx * .1;
 			const rot = time * speed;
 			cube.rotation.x = rot;
 			cube.rotation.y = rot;
+            if(man){
+                man.rotation.y = rot;
+            }
 
 		} );
+
+        
 
 		renderer.render( scene, camera );
 
